ad581de0f89b4b7331cb1efdb2ef0deff7ea0bda
Shruti Ranjan Jha <72469934+shruti724@users.noreply.github.com>
2023-02-07 23:40:48 +0530

    Update DNF_sort012.py

---
+++ Module2_Arrays/DNF_sort012.py
@@ -0,0 +1,20 @@
+def sort012(arr, n):
+
+    low = 0
+    mid = 0
+    high = n - 1
+    while mid <= high:
+            if arr[mid] == 0:
+                arr[mid], arr[low] = arr[low], arr[mid]
+                mid += 1
+                low += 1
+    
+            elif arr[mid] == 2:
+                arr[mid], arr[high] = arr[high], arr[mid]
+                high -= 1
+            else:
+                mid += 1
+                
+  arr = [0, 1, 2, 2, 0, 1]
+  n = len(arr)
+  print(sort012(arr, n))
